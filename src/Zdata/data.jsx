898a6b2673c87c18eef8163754f407610d07f910
arase <23aw0103@jec.ac.jp>
2024-01-27 10:12:49 +0900

    1

--- src/Zdata/data.jsx
+++ src/Zdata/data.jsx
@@ -199,12 +199,12 @@ export const footeroverview = [
 ]
 export const footerdownloadbtn = [
   {
-    id: 'downloadbutton-app',
+    id: 'fdownloadbutton-app',
     link: '/img/downloadbutton-app.png',
     url: 'https://apps.apple.com/jp/app/%E3%82%B9%E3%83%9D%E3%82%AB%E3%83%AC/id1337649882',
   },
   {
-    id: 'downloadbutton-google',
+    id: 'fdownloadbutton-google',
     link: '/img/downloadbutton-google.png',
     url: 'https://play.google.com/store/apps/details?id=com.spocale.android',
   },
